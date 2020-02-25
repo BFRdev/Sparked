d8de620b2b246f9249638ea6bef919d6ef8e6e15
Ameer <ameeryasin17@gmail.com>
2020-02-25 18:35:43 -0500

    finished category list for signing up

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import CategoryList from './components/categoryList';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Changed Text homies</Text>
+      {/* <Text>Open up App.js to start working on your app!</Text>
+      <Text>Changed Text homies</Text> */}
+      <CategoryList></CategoryList>
     </View>
   );
 }
--- components/categoryList.js
+++ components/categoryList.js
@@ -1,10 +1,112 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, TouchableOpacity, FlatList } from 'react-native';
 
-const CategoryList = () => {
-    return( 
-        <View></View>
+import Constants from 'expo-constants';
+
+
+//data array 
+const categories = [
+    {
+        id: '01',
+        title: 'fitness',
+    },
+    {
+        id: '02',
+        title: 'mindfullness',
+    },
+    {
+        id: '03',
+        title: 'education',
+    },
+    {
+        id: '04',
+        title: 'addict free',
+    },
+    {
+        id: '05',
+        title: 'creativity',
+    },
+    {
+        id: '06',
+        title: 'relationships',
+    },
+    {
+        id: '07',
+        title: 'finance',
+    },
+    {
+        id: '08',
+        title: 'travel',
+    },
+];
+
+function Item({ id, title, selected, onSelect }) {
+    return (
+      <TouchableOpacity
+        onPress={() => onSelect(id)}
+        style={[
+          styles.item,
+          { backgroundColor: selected ? '#EF9D53' : '#51BA65' },
+        ]}
+      >
+        <Text style={styles.title}>{title}</Text>
+      </TouchableOpacity>
     );
-}
+  }
+  
+  export default function CategoryList() {
+    const [selected, setSelected] = React.useState(new Map());
+  
+    const onSelect = React.useCallback(
+      id => {
+        const newSelected = new Map(selected);
+        newSelected.set(id, !selected.get(id));
+  
+        setSelected(newSelected);
+      },
+      [selected],
+    );
+  
+    return (
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={categories}
+          renderItem={({ item }) => (
+            <Item
+              id={item.id}
+              title={item.title}
+              selected={!!selected.get(item.id)}
+              onSelect={onSelect}
+            />
+          )}
+          numColumns={2}
+          keyExtractor={item => item.id}
+          extraData={selected}
+        />
+      </SafeAreaView>
+    );
+  }
 
-export default CategoryList
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginVertical: 20,
+      marginTop: Constants.statusBarHeight,
+    },
+    item: {
+      backgroundColor: '#EF9D53',
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+    },
+    title: {
+      fontSize: 20,
+      textAlign: 'center',
+    },
+  });
+
+
+
+
+
+
