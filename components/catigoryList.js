6acfb2fb01cb0b9f6776d87b1d7e0b6699a6d6d3
Ameer <ameeryasin17@gmail.com>
2020-02-25 17:23:13 -0500

    component

---
+++ components/catigoryList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
+
+const CatigoryList = () => {
+    return( 
+        <View></View>
+    );
+}
+
+export default CatigoryList
