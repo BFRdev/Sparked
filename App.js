d54d73a5aaf1f18f8b0d32813feb142c13982779
Michael Chaya <michaelchaya@Michaels-MacBook-Pro.local>
2020-02-26 01:26:18 -0500

    Sign in, Sign up, Logout now functional.

--- App.js
+++ App.js
@@ -7,6 +7,41 @@ import HomeScreen from './screens/HomeScreen'
 
 import * as firebase from 'firebase'
 
+var firebaseConfig = {
+  apiKey: "AIzaSyCN8ZmOhyI2upGCWpPzMZsqmEY_igIvrgc",
+  authDomain: "sparked-bd638.firebaseapp.com",
+  databaseURL: "https://sparked-bd638.firebaseio.com",
+  projectId: "sparked-bd638",
+  storageBucket: "sparked-bd638.appspot.com",
+  messagingSenderId: "480536595337",
+  appId: "1:480536595337:web:945f6e133d77bc19a66f58",
+  measurementId: "G-YGGLN3DBXH"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
+const Appstack = createStackNavigator({
+  Home: HomeScreen
+})
+
+const AuthStack = createStackNavigator({
+  Login: LoginScreen,
+  Register: RegisterScreen
+})
+
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      Loading: LoadingScreen,
+      App: Appstack,
+      Auth: AuthStack
+    },
+    {
+      initialRouteName: "Loading"
+    }
+  )
+);
 
 
 
