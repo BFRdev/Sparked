e3b0e379b769835cb84bc706f7cf1a41ce6ebdd7
Ameer <ameeryasin17@gmail.com>
2020-03-07 18:50:02 -0500

    reupload

--- screens/AddGoalScreen.jsx
+++ screens/AddGoalScreen.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Center} from '../components/Center'
 
 export const AddGoalScreen = ({}) => {
     return( 
--- screens/GoalsScreen.jsx
+++ screens/GoalsScreen.jsx
@@ -1,26 +1,18 @@
 import React from 'react'
 import { Center } from '../components/Center'
 import { Text, TouchableOpacity, SafeAreaView, StyleSheet, FlatList, View } from 'react-native'
-import { createStackNavigator } from '@react-navigation/stack';
-
-import AddGoalScreen from './AddGoalScreen';
 
 import GoalsList from '../components/goalList';
-import { NavigationActions } from 'react-navigation';
-
-const Stack = createStackNavigator();
 
 
-export const GoalsScreen = ({ }) => {
+export const GoalsScreen = ({navigation}) => {
 
     return (
         <SafeAreaView style={styles.container}>
             {/* header title */}
             <Text style={styles.goalText}> Your Goals</Text>
             {/* add goal button goto stack */}
-            <TouchableOpacity onPress={()=> { navigation.navigate("addGoal", {
-                        name: goal
-                    }); }} >
+            <TouchableOpacity onPress={()=> navigation.push("AddGoal")} >
                 <Text style={styles.addBtn}> + </Text>
             </TouchableOpacity>
 
@@ -30,19 +22,6 @@ export const GoalsScreen = ({ }) => {
     );
 
 }
-
-export const GoalStackNav = ({ }) => {
-
-    return(
-    <Stack.Navigator initialRouteName="goalsScreen">
-        <Stack.Screen name="goalsScreen" component={GoalsScreen}/>
-        <Stack.Screen name="addGoal" component={AddGoalScreen}/>
-
-    </Stack.Navigator>
-    );
-}
-
-
 
 
 const styles = StyleSheet.create({
