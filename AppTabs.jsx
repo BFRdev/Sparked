903af7585a89e3591ff9ef2d1d1b0f9515f680fe
Ameer <ameeryasin17@gmail.com>
2020-03-07 14:00:30 -0500

    removed header

--- AppTabs.jsx
+++ AppTabs.jsx
@@ -12,50 +12,55 @@ import ProfileScreen from './screens/ProfileScreen';
 import { LaunchScreen } from './screens/LaunchScreen';
 
 //const Stack = createStackNavigator()
-
-
-//declare tab-nav
 const Tabs = createBottomTabNavigator()
 
+export default class AppTabs extends React.Component {
+    // remove stack header
+    static navigationOptions = {
+        headerShown: false,
+    };
+    render() {
+        return (
 
-export const AppTabs = ({ }) => {
+            <NavigationContainer>
+                <Tabs.Navigator
+                    // configerations 
+                    screenOptions={({ route }) => ({
+                        tabBarIcon: ({ color, size }) => {
+                            let iconName;
+
+                            if (route.name === 'Goals') {
+                                return <MaterialIcons name={"playlist-add-check"} size={size} color={color} />;
+                            } else if (route.name === 'Feed') {
+                                return <FontAwesome name={"feed"} size={size} color={color} />;
+                            } else if (route.name === 'Launch') {
+                                return <FontAwesome name={"rocket"} size={size} color={color} />;
+                            }
+                            else if (route.name === 'Search') {
+                                return <FontAwesome name={"search"} size={size} color={color} />;
+                            } else if (route.name === 'Profile') {
+                                return <Ionicons name={"md-person"} size={size} color={color} />;
+                            }
+                        },
+                    })}
+                    tabBarOptions={{
+                        activeTintColor: '#EF9D53',
+                        inactiveTintColor: '#0D52BA',
+                    }}
+                >
+                    {/* screen routes */}
+                    <Tabs.Screen name='Goals' component={GoalsScreen} />
+                    <Tabs.Screen name='Feed' component={FeedScreen} />
+                    <Tabs.Screen name='Launch' component={LaunchScreen} />
+                    <Tabs.Screen name='Search' component={SearchScreen} />
+                    <Tabs.Screen name='Profile' component={ProfileScreen} />
+                </Tabs.Navigator>
+            </NavigationContainer>
 
 
-    return (
+        );
+    }
 
-        <NavigationContainer>
-            <Tabs.Navigator
-                // configerations 
-                screenOptions={({ route }) => ({
-                    tabBarIcon: ({ color, size }) => {
-                        let iconName;
-
-                        if (route.name === 'Goals') {
-                            return <MaterialIcons name={"playlist-add-check"} size={size} color={color} />;
-                        } else if (route.name === 'Feed') {
-                            return <FontAwesome name={"feed"} size={size} color={color} />;
-                        } else if (route.name === 'Launch') {
-                            return <FontAwesome name={"rocket"} size={size} color={color} />;
-                        }
-                        else if (route.name === 'Search') {
-                            return <FontAwesome name={"search"} size={size} color={color} />;
-                        } else if (route.name === 'Profile') {
-                            return <Ionicons name={"md-person"} size={size} color={color} />;
-                        }
-                    },
-                })}
-                tabBarOptions={{
-                    activeTintColor: '#EF9D53',
-                    inactiveTintColor: '#0D52BA',
-                }}
-            >
-                {/* screen routes */}
-                <Tabs.Screen name='Goals' component={GoalsScreen} />
-                <Tabs.Screen name='Feed' component={FeedScreen} />
-                <Tabs.Screen name='Launch' component={LaunchScreen} />
-                <Tabs.Screen name='Search' component={SearchScreen} />
-                <Tabs.Screen name='Profile' component={ProfileScreen} />
-            </Tabs.Navigator>
-        </NavigationContainer>
-    );
 }
+
+//declare tab-nav
